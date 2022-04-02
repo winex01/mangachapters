01b34ddb8c8075b74c01b456e6e84e887289846e
winex01 <winnie131212592@gmail.com>
2022-04-02 12:34:29 +0800

    add manga scaffolding

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -16,4 +16,5 @@ Route::group([
 ], function () { // custom admin routes
     Route::crud('audittrail', 'AuditTrailCrudController');
     Route::crud('menu', 'MenuCrudController');
+    Route::crud('manga', 'MangaCrudController');
 }); // this should be the absolute last line of this file

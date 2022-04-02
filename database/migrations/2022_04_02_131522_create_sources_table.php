574d649d9a4ad6415bc8153e6f4760f3c3b16ebb
winex01 <winnie131212592@gmail.com>
2022-04-02 15:40:28 +0800

    fix migration fk column in sources table

--- database/migrations/2022_04_02_131522_create_sources_table.php
+++ database/migrations/2022_04_02_131522_create_sources_table.php
@@ -21,8 +21,6 @@ class CreateSourcesTable extends Migration
 
             $table->boolean('published')->default(false);// false
 
-            $table->string('crawler_filter')->nullable();
-
             $table->softDeletes();
             $table->timestamps();
         });

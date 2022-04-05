e3f437ffec04cdeb839ac6ec7bdf77217c08025b
winex01 <winnie131212592@gmail.com>
2022-04-05 15:10:00 +0800

    add schedule backup and scan command in kerner console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ScanChapters;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        ScanChapters::class,
     ];
 
     /**
@@ -24,7 +25,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('winex:scan-chapters')->hourly();
+
+        $schedule->command('backup:clean')->daily()->at('11:00');
+        $schedule->command('backup:run')->daily()->at('12:00');
     }
 
     /**

73e4ef0d2b163e0a5723cfa0a3a2df869f2e077f
winex01 <winnie131212592@gmail.com>
2022-04-08 17:13:03 +0800

    show only invalid links export to user who has access rights

--- app/Http/Controllers/Admin/ChapterCrudController.php
+++ app/Http/Controllers/Admin/ChapterCrudController.php
@@ -171,4 +171,3 @@ class ChapterCrudController extends CrudController
 }
 // TODO:: add chapters table for guest
 // TODO:: allow user to report chapter in chapters crud as invalid link
-// TODO:: dont user invalid link in manga chaptesr report

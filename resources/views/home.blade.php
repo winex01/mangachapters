72f2ce082d80e92408d47c1d59f1fda18fb954af
winex01 <winnie131212592@gmail.com>
2022-04-15 14:41:51 +0800

    wip footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,110 +1,77 @@
-@extends('layouts.guest_main')
+@extends('layouts.guest_blank')
 
-@section('content')
+@section('guest_blank_content')
 
-<main role="main" class="container">
-  
-  <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-dark rounded shadow-sm">
-    <a href="/">
-      <img class="mr-3" src="{{ asset('images/logo.svg') }}" alt="" width="50" height="50">
-    </a>
-    <div class="lh-100">
-      <a href="/" style="text-decoration: none;">
-        <h6 class="mb-0 text-white lh-100">{{ config('appsettings.app_name') }}</h6>
-      </a>
-      <small>{!! trans('lang.slogan') !!}</small>
-    </div>
-  </div>
+  <h6 class="border-bottom border-gray pb-2 mb-0">{{ trans('lang.chapter_recent_chapters') }}</h6>
 
-  
+  @foreach ($chapters->chunk(3) as $chunks)
 
-  {{-- BEGIN CHAPTERS --}}
-  <div class="my-3 p-3 bg-white rounded shadow-sm">
-    <div class="float-right">
-      <a class="text-primary" href="/login">Login</a>
-      <a class="text-success" href="/register">Register</a>
-    </div>
+    <div class="row">
 
-    <h6 class="border-bottom border-gray pb-2 mb-0">{{ trans('lang.chapter_recent_chapters') }}</h6>
+      @foreach ($chunks as $chapter)
 
-    @foreach ($chapters->chunk(3) as $chunks)
-
-      <div class="row">
-
-        @foreach ($chunks as $chapter)
-
-          <x-chapter-card :chapter="$chapter"></x-chapter>
-          
-        @endforeach
+        <x-chapter-card :chapter="$chapter"></x-chapter>
         
-      </div>
-    @endforeach
-
-    <small class="d-block text-right mt-3">
-      {{ $chapters->links() }}
-    </small>
-
-  </div>
-  {{-- END chapters --}}
-
-
-  <div class="my-3 p-3 bg-white rounded shadow-sm">
-    <h6 class="border-bottom border-gray pb-2 mb-0">{{ __('Random mangas') }}</h6>
-
-
-    @foreach ($mangas->chunk(3) as $chunks)
-      <div class="row">
-
-        @foreach ($chunks as $manga)
-          
-        <div class="chapter-card media text-muted pt-3 mb-n2 col-md-4">
-          <img style="height: 55px; width:40px;" src="{{ $manga->photo }}" class="mt-1 rounded" alt="...">
-          <div class="flexbox ml-2 media-body small border-bottom border-gray">
-              
-              <a class="d-block text-muted font-weight-bold" href="{{ backpack_url('manga/'.$manga->id.'/show') }}" >
-                {{ $manga->title }}
-              </a>
-            
-              @php
-                  $count = 0;
-              @endphp
-              @foreach ($manga->chapters as $chapter)
-                  @php
-                      $count++;
-                  @endphp
-                  {!! anchorNewTab($chapter->url, trans('lang.chapter_description', [
-                    'chapter' => $chapter->chapter, 
-                    'release' => $chapter->release, 
-                  ]) ) !!}
-                  <br>
-              @endforeach
-
-              @if ($count < 2)
-                  <br>                  
-              @endif
+      @endforeach
       
-          </div>
-        </div>
-      
+    </div>
+  @endforeach
 
-        @endforeach
-        
-      </div>
-    @endforeach
-
-    <small class="d-block text-right mt-3">
-      <a href="/manga">{{ __('See all mangas') }}</a>
-    </small>
-  </div>
-
-  
-
-
-
-</main>
+  <small class="d-block text-right mt-3">
+    {{ $chapters->links() }}
+  </small>
 
 @endsection
 
 
-@push('after_scripts')
-@endpush
+@section('guest_blank_after_content')
+<div class="my-3 p-3 bg-white rounded shadow-sm">
+  <h6 class="border-bottom border-gray pb-2 mb-0">{{ __('Random mangas') }}</h6>
+
+    @foreach ($mangas->chunk(3) as $chunks)
+      <div class="row">
+
+          @foreach ($chunks as $manga)
+            
+              <div class="chapter-card media text-muted pt-3 mb-n2 col-md-4">
+                <img style="height: 55px; width:40px;" src="{{ $manga->photo }}" class="mt-1 rounded" alt="...">
+                <div class="flexbox ml-2 media-body small border-bottom border-gray">
+                    
+                    <a class="d-block text-muted font-weight-bold" href="{{ backpack_url('manga/'.$manga->id.'/show') }}" >
+                      {{ $manga->title }}
+                    </a>
+                  
+                    @php
+                        $count = 0;
+                    @endphp
+                    @foreach ($manga->chapters as $chapter)
+                        @php
+                            $count++;
+                        @endphp
+                        {!! anchorNewTab($chapter->url, trans('lang.chapter_description', [
+                          'chapter' => $chapter->chapter, 
+                          'release' => $chapter->release, 
+                        ]) ) !!}
+                        <br>
+                    @endforeach
+
+                    @if ($count < 2)
+                        <br>                  
+                    @endif
+            
+                </div>
+              </div>
+
+          @endforeach
+              
+      </div>
+    @endforeach
+
+  <small class="d-block text-right mt-3">
+    <a href="/manga">{{ __('See all mangas') }}</a>
+  </small>
+</div>
+
+
+    
+@endsection
---
+++ resources/views/layouts/guest_blank.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.guest_main')
+
+@section('content')
+
+<main role="main" class="container">
+  
+  <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-dark rounded shadow-sm">
+    <a href="/">
+      <img class="mr-3" src="{{ asset('images/logo.svg') }}" alt="" width="50" height="50">
+    </a>
+    <div class="lh-100">
+      <a href="/" style="text-decoration: none;">
+        <h6 class="mb-0 text-white lh-100">{{ config('appsettings.app_name') }}</h6>
+      </a>
+      <small>{!! trans('lang.slogan') !!}</small>
+    </div>
+  </div>
+
+  
+
+  <div class="my-3 p-3 bg-white rounded shadow-sm">
+    <div class="float-right">
+      <a class="text-primary" href="/login">Login</a>
+      <a class="text-success" href="/register">Register</a>
+    </div>
+
+    @yield('guest_blank_content')
+
+  </div>
+
+
+    @yield('guest_blank_after_content')
+    
+
+    <div class="my-3 p-3 bg-white rounded shadow-sm">
+      <div class="bd-footer text-muted">
+        <div class="container-fluid p-3 p-md-5">
+          <ul class="bd-footer-links">
+            <li><a href="https://github.com/twbs/bootstrap">GitHub</a></li>
+            <li><a href="https://twitter.com/getbootstrap">Twitter</a></li>
+            <li><a href="/docs/4.1/examples/">Examples</a></li>
+            <li><a href="/docs/4.1/about/overview/">About</a></li>
+          </ul>
+          <p>Designed and built with all the love in the world by <a href="https://twitter.com/mdo" target="_blank" rel="noopener">@mdo</a> and <a href="https://twitter.com/fat" target="_blank" rel="noopener">@fat</a>. Maintained by the <a href="https://github.com/orgs/twbs/people">core team</a> with the help of <a href="https://github.com/twbs/bootstrap/graphs/contributors">our contributors</a>.</p>
+          <p>Currently v4.1.3. Code licensed <a href="https://github.com/twbs/bootstrap/blob/main/LICENSE" target="_blank" rel="license noopener">MIT</a>, docs <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="license noopener">CC BY 3.0</a>.</p>
+        </div>
+      </div>
+    </div>
+
+</main>
+
+@endsection

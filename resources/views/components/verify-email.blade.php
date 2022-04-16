82709ca3619a47605de7f7d0800c9e4fec4225c4
winex01 <winnie131212592@gmail.com>
2022-04-17 00:52:41 +0800

    create verify-email component

---
+++ resources/views/components/verify-email.blade.php
@@ -0,0 +1,20 @@
+@if (!auth()->user()->hasVerifiedEmail())
+    <div class="card">
+        <div class="card-header">{{ __('Verify Your Email Address (Optional)') }}</div>
+        
+        <div class="card-body">
+            @if (session('message'))
+            <div class="alert alert-success" role="alert">
+                {{ __('A fresh verification link has been sent to your email address.') }}
+            </div>
+            @endif
+            
+            {{ __('Before proceeding, please check your email for a verification link.') }}
+            {{ __('If you did not receive the email') }},
+            <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
+                @csrf
+                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+            </form>
+        </div>
+    </div>
+@endif
--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -2,26 +2,7 @@
 
 @section('content')
 
-    @if (!auth()->user()->hasVerifiedEmail())
-        <div class="card">
-            <div class="card-header">{{ __('Verify Your Email Address (Optional)') }}</div>
-            
-            <div class="card-body">
-                @if (session('message'))
-                <div class="alert alert-success" role="alert">
-                    {{ __('A fresh verification link has been sent to your email address.') }}
-                </div>
-                @endif
-                
-                {{ __('Before proceeding, please check your email for a verification link.') }}
-                {{ __('If you did not receive the email') }},
-                <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-                    @csrf
-                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                </form>
-            </div>
-        </div>
-    @endif
+    <x-verify-email></x-verify-email> 
     
     @can('admin_widgets')
         @include(backpack_view('inc.custom_my_widgets'))

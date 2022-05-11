45223dd656ebb2da14e87ec76dcb290397750527
winex01 <winnie131212592@gmail.com>
2022-05-11 13:19:36 +0800

    add discord manga notification

---
+++ app/Notifications/DiscordNewMangaNotification.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Discord\DiscordChannel; 
+use NotificationChannels\Discord\DiscordMessage; 
+
+class DiscordNewMangaNotification extends Notification
+{
+    use Queueable;
+
+    protected $manga;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($manga)
+    {
+        $this->manga = $manga;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [DiscordChannel::class];
+    }
+
+    public function toDiscord($notifiable)
+    {
+        return DiscordMessage::create($this->getMessage());
+    }
+
+    private function getMessage() 
+    {
+        return 'New '.$this->manga->type->name.': '.$this->manga->title.' - '.$this->manga->alternative_title;
+    }
+}

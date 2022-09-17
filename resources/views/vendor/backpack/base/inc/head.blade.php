340fb1055998525f3bfb10a570cab50d44e22404
winex01 <winnie131212592@gmail.com>
2022-09-17 18:57:27 +0800

    add adstera ads

--- resources/views/vendor/backpack/base/inc/head.blade.php
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -31,3 +31,6 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    {{-- adstera, please check also base>inc>ads.blade.php --}}
+    <script type='text/javascript' src='//pl17716795.profitablegatetocontent.com/1f/4f/1c/1f4f1c3d7ee17d0a2e9bef95cedb8246.js'></script>

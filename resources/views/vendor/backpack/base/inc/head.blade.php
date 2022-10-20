e50cc3a6e074838af9289a7b203afa231f05e657
winex01 <winnie131212592@gmail.com>
2022-10-20 13:16:06 +0800

    add purple ads

--- resources/views/vendor/backpack/base/inc/head.blade.php
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -31,6 +31,10 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    {{-- purpleads --}}
+    <meta name="purpleads-verification" content="7e18bed9ea90478631c42e19"/>
+
 
     {{-- adstera, please check also base>inc>ads.blade.php --}}
     {{-- popunder --}}

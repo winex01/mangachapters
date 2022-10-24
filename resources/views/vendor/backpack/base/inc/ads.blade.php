a381662b5df498a9edb79e8503d194c8f5792c4d
winex01 <winnie131212592@gmail.com>
2022-10-24 21:54:29 +0800

    add adsterra social bar ad format

--- resources/views/vendor/backpack/base/inc/ads.blade.php
+++ resources/views/vendor/backpack/base/inc/ads.blade.php
@@ -3,3 +3,6 @@
 {{-- NativeBanner_1 --}}
 <script async="async" data-cfasync="false" src="//pl17716719.profitablegatetocontent.com/aa9d00f330b38584760fa45180d41e9d/invoke.js"></script>
 <div id="container-aa9d00f330b38584760fa45180d41e9d"></div>
+
+{{-- Social Bar --}}
+<script type='text/javascript' src='//pl17850124.profitablegatetocontent.com/82/f8/53/82f853c5988ecb407bae57851ec38e40.js'></script>
